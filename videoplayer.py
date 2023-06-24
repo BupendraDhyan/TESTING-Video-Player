5add672f2ec603a505b8d03dac0eb8ba5ef3f43c
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-06-24 10:27:24 +0530

    Update videoplayer.py

--- videoplayer.py
+++ videoplayer.py
@@ -4,4 +4,12 @@ from tkinter import ttk
 from tkinter import filedialog
 
 root = tk.Tk()
+root.title("Video Player")
+root.geometry("800x600")
 
+video_frame = ttk.Frame(root)
+video_frame.place(x=5, y=5, width=790, height=490)
+
+media_player = vlc.MediaPlayer()
+play_button = ttk.Button(root, text="PLAY", command=lambda: media_player.play())
+play_button.place(x=5, y=500, width=100)
