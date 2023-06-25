82c17c7c40c1b3d01776a510a6e83652d67cbc77
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-06-25 13:34:35 +0530

    Update videoplayer.py

--- videoplayer.py
+++ videoplayer.py
@@ -13,3 +13,12 @@ video_frame.place(x=5, y=5, width=790, height=490)
 media_player = vlc.MediaPlayer()
 play_button = ttk.Button(root, text="PLAY", command=lambda: media_player.play())
 play_button.place(x=5, y=500, width=100)
+
+pause_button = ttk.Button(root, text="PAUSE", command=lambda: media_player.pause())
+pause_button.place(x=110, y=500, width=100)
+
+stop_button = ttk.Button(root, text="STOP", command=lambda: media_player.stop())
+stop_button.place(x=215, y=500, width=100)
+
+open_button = ttk.Button(root, text="OPEN", command=lambda: open_file())
+open_button.place(x=320, y=500, width=100)
